a99a3863941d93c1b5c46c48f3a70c672dcfd3ef
Bonface Shisakha <bonface.developer@gmail.com>
2018-02-05 12:41:47 +0300

    Remove main_app folder

--- main_app/api/routes.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-
-mod_api = Blueprint('api', __name__,)
-
-@mod_api.route('/getstuff')
-def getStuff():
-    return '{"greetings" : "Hello user"}'
--- main_app/rans_website/routes.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-
-mod_website = Blueprint('rans_website', __name__,)
-
-@mod_website.route('/home')
-def index():
-    return '<h1>You are on the home page </h1>'
